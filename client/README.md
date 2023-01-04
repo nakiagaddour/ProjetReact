5c79971ccfcb001ee6631b2ca2d06433f44b9ad1
nakiagaddour <nakiagaddour742@gmail.com>
2023-01-04 09:55:17 +0100

    init push

---
+++ client/README.md
@@ -0,0 +1,7 @@
+Hooks utilisé : 
+useEffect : pour la creation de post dans la partie Form 
+useState : etat de post actuel a partir de setCurrentId 
+useSelecter : selectionne currentId / message de  post=> update 
+useDispatch : Like / delete post dans la partie Post 
+useStyles : pour les classes 
+redux : Est une librairie de gestion de l’état de l’application
---
+++ client/src/styles.js
@@ -0,0 +1,20 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(() => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    background:'rgb(255, 255, 153)'
+  },
+  heading: {
+    color:'black',
+  },
+  image: {
+    marginLeft: '30px',
+    
+  },
+}));
